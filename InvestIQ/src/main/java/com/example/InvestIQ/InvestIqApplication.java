609098c4d462f13422db0844905cee06650f4caa
Siddharthkummari <kummarisiddharth08@gmail.com>
2026-08-01 06:48:34 +0000

    initial commit

---
+++ InvestIQ/src/main/java/com/example/InvestIQ/InvestIqApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvestIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestIqApplication.class, args);
+	}
+
+}
